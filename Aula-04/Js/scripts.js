8a6d79a3952bb54a5143c8fa67210f353ce3415c
Jeferson Oliveira <oliveira.jeferson.jso@gmail.com>
2022-06-22 20:08:17 -0300

    More the a words

---
+++ Aula-04/Js/scripts.js
@@ -0,0 +1,9 @@
+let cavalo = {
+  patas: 4,
+  "esta domesticado": false,
+  estaDomesticado: false,
+};
+
+console.log(cavalo);
+console.log(cavalo["esta domesticado"]);
+console.log(cavalo.estaDomesticado);
