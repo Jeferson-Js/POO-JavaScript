96128ab9de246ff6664cf761052776657d54740d
Jeferson Oliveira <oliveira.jeferson.jso@gmail.com>
2022-06-23 19:00:56 -0300

    deleting methods and properties in objects

---
+++ Aula-10/Js/scripts.js
@@ -0,0 +1,12 @@
+let carro = {
+  tipo: "SUV",
+  ligar: function () {
+    console.log("ligado");
+  },
+};
+
+//delete carro.tipo; // comando utilizado para deletar propriedades e metodos.
+//delete carro.ligar; // comando utilizado para deletar propriedades e metodos.
+
+console.log(carro.tipo);
+carro.ligar();
