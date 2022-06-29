b7ce9693f04f37b7cc0d34fe6512edf60de79841
Jeferson Oliveira <oliveira.jeferson.jso@gmail.com>
2022-06-29 09:33:52 -0300

    Atualização de metodos do projeto loja

--- Exercicio-06/Js/scripts.js
+++ Exercicio-06/Js/scripts.js
@@ -2,12 +2,17 @@ class Produto {
   constructor() {
     this.id = 1;
     this.arrayProdutos = [];
+    this.editId = null;
   }
   salvar() {
     let produto = this.lerDados();
 
     if (this.validaCampos(produto)) {
-      this.adicionar(produto);
+      if (this.editId == null) {
+        this.adicionar(produto);
+      } else {
+        this.atualizar(this.editId, produto);
+      }
     }
 
     this.listaTabela();
@@ -32,19 +37,45 @@ class Produto {
 
       let imgEdit = document.createElement("img");
       imgEdit.src = "img/editar.png";
+      imgEdit.setAttribute(
+        "onclick",
+        "produto.preparaEdicao(" + JSON.stringify(this.arrayProdutos[i]) + ")"
+      );
 
       td_acoes.appendChild(imgEdit);
 
       let imgDelet = document.createElement("img");
       imgDelet.src = "img/delet.png";
+      imgDelet.setAttribute(
+        "onclick",
+        "produto.deletar(" + this.arrayProdutos[i].id + ")"
+      );
 
       td_acoes.appendChild(imgDelet);
     }
   }
 
   adicionar(produto) {
+    produto.preco = parseFloat(produto.preco);
     this.arrayProdutos.push(produto);
     this.id++;
+  }
+
+  atualizar(id, produto) {
+    for (let i = 0; i < this.arrayProdutos.length; i++) {
+      if (this.arrayProdutos[i].id == id) {
+        this.arrayProdutos[i].nomeProduto = produto.nomeProduto;
+        this.arrayProdutos[i].preco = produto.preco;
+      }
+    }
+  }
+
+  preparaEdicao(dados) {
+    this.editId = dados.id;
+    document.getElementById("produto").value = dados.nomeProduto;
+    document.getElementById("preco").value = dados.preco;
+
+    document.getElementById("salvar").innerText = "Atualizar";
   }
 
   lerDados() {
@@ -74,6 +105,21 @@ class Produto {
   cancelar() {
     document.getElementById("produto").value = "";
     document.getElementById("preco").value = "";
+
+    document.getElementById("salvar").innerText = "Salvar";
+    this.editId = null;
+  }
+  deletar(id) {
+    if (confirm("Você realmente quer deletar este produto? " + id)) {
+      let tbody = document.getElementById("tbody");
+
+      for (let i = 0; i < this.arrayProdutos.length; i++) {
+        if (this.arrayProdutos[i].id == id) {
+          this.arrayProdutos.splice(i, 1);
+          tbody.deleteRow(i);
+        }
+      }
+    }
   }
 }
 
